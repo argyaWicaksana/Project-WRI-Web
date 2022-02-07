45a69604fde667410dd6ec269c2e1ac154c3e7ba
argyaWicaksana <argya7@gmail.com>
2022-02-07 23:46:35 +0700

    check everything is ok

--- Navigation Bar/script.js
+++ Navigation Bar/script.js
@@ -6,10 +6,6 @@ menuToggle.addEventListener('click', function ( ) {
 });
 
 function change(form) {
-   // document.getElementById('username').innerHTML= "Email"
-   // document.getElementsByName('plcholder')[0].placeholder= 'Enter Email'
-   // document.getElementById('repeatPwd').style.display='block'
-   // document.getElementById('login').innerHTML= "Sign Up"
    if (form) {
       document.getElementById('loginForm').style.display='none'
       document.getElementById('signUpForm').style.display='block'
@@ -19,8 +15,6 @@ function change(form) {
    }
 }
 
-function displaySty(displayStyle, clr) {
+function displaySty(displayStyle) {
    document.getElementById("background").style.display=displayStyle
-   document.getElementsByName("body")[0].style.backgroundColor=clr
-   console.log(clr)
 }
